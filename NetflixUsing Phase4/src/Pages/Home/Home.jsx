0b27db6c597224bdc71bc678253d452772e7ce4a
Fetu Deen <fettabin@gmail.com>
2024-09-12 23:11:36 +0300

    Banner

--- NetflixUsing Phase4/src/Pages/Home/Home.jsx
+++ NetflixUsing Phase4/src/Pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./Home.css";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Banner from "../../Components/Banner/Banner";
 function Home() {
   return (
     <>
       <Header />
+      <Banner/>
       <Footer />
     </>
   );
